382480e206c550765287bad9a14603c63a8e6917
hiro-hero0708 <hiroto435043@gmail.com>
2019-11-03 14:13:09 +0900

    update グループ編集機能

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -20,4 +20,22 @@ class GroupsController < ApplicationController
     def group_params
         params.require(:group).permit(:name, user_ids: [] )
     end
+
+    def update
+        if @group.update(group_params)
+            redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+          else
+            render :edit
+          end
+        end
+      
+        private
+      
+        def group_params
+          params.require(:group).permit(:name, { :user_ids => [] })
+        end
+      
+        def set_group
+          @group = Group.find(params[:id])
+        end
 end
